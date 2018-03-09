dee4066a82c3ea4e9a0b1202b701e60320b65c48
gillou_g <guillaume.gilloux@epitech.eu>
2018-03-09 08:39:42 +0100

    jinja2 implementation

--- site/index.py
+++ site/index.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 import sys
-from templates import index
 from flask import Flask, render_template, request, json, jsonify
-from flaskext.mysql import MySQL
+from jinja2 import Template
+import time
 
 app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def idx():
-    return index.main()
+    return render_template('index.html', email=(str(time.time() / 1000) + "@hacktonhub.com"))
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5001)
