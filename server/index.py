23948803000ca7b403d6424faddbcfc4abd66f72
thamin <guillaumegilloux@hotmail.fr>
2018-03-09 07:47:47 +0000

    last version

--- server/index.py
+++ server/index.py
@@ -2,14 +2,13 @@
 
 import sys
 from flask import Flask, render_template, request, json, jsonify
-from jinja2 import Template
-import time
+from flaskext.mysql import MySQL
 
 app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def idx():
-    return render_template('index.html', email=(str(time.time() / 1000) + "@hacktonhub.com"))
+    return "Subway generator here"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
